34abd3e297eaaa40ae191e233a66cc37fead195c
Josh Auerbach <joshauerbachwatson@gmail.com>
2025-06-27 17:56:00 -0400

    Initial project for unified app
    Issue #1

---
+++ unigame-app/unigame_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  unigame_appApp.swift
+//  unigame-app
+//
+//  Created by Josh Auerbach on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct unigame_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
